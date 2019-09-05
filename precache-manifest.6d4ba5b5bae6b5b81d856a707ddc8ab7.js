a8301287aec6925494e1683bf45eb150db47cc87
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-05 18:07:18 +0300

    Updates

---
+++ precache-manifest.6d4ba5b5bae6b5b81d856a707ddc8ab7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75abbb0ae03abcffa7c8943b8336dff5",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "af267896607cf73f41e7",
+    "url": "/shopping-cart/static/css/main.053a31c6.chunk.css"
+  },
+  {
+    "revision": "eb5a89f498eb1cf24517",
+    "url": "/shopping-cart/static/js/2.b151a890.chunk.js"
+  },
+  {
+    "revision": "af267896607cf73f41e7",
+    "url": "/shopping-cart/static/js/main.6e023308.chunk.js"
+  },
+  {
+    "revision": "7bc807bdb81a0e471469",
+    "url": "/shopping-cart/static/js/runtime~main.dd799ef7.js"
+  }
+]);
